f53ca60267fc8881130153526ef71799dab41d72
su apt install Kuma <maxxozou@gmail.com>
2023-03-09 09:01:57 +0100

    Ver 1.26.2

--- script/getScore.js
+++ script/getScore.js
@@ -7,39 +7,46 @@ var couleur = '';
 //-- Récupère le tableau sur la page HTML ------------
 const tableau = document.getElementById('tableau')
 
-//-- Ajoute au tableau les scores sauvegardé ------------
-for(let i = 0; i < scores.length; i++) {
-
-    //-- Pour rendre le tableau plus beau ------------
-    switch(scores[i].difficulte) {
-        case 'bleusaille':
-            difficulte = 'Bleusaille';
-            break;
-    
-        case 'violence':
-            difficulte = 'Violence';
-            break;
-    
-        case 'ultraViolence':
-            difficulte = 'Ultra-Violence';
-            break;
-    
-        case 'cauchemar':
-            difficulte = 'Cauchemar';
-            break;
-    
-        case 'ultraCauchemar':
-            difficulte = 'Ultra-Cauchemar';
-            break;
-    }
-
-    //-- Change la couleur du fond pour qu'il soit plus compréhensible ------------
-    if(scores[i].status === 'Gagné') {
-        couleur = 'green';
-    } else {
-        couleur = 'red'
-    }
-
+//-- Si aucune partie n'as été joué il affiche une ligne de non-score, sinon il les rajoute------------
+if(scores.length === 0) {
     //-- Insert la ligne dans le tableau ------------
-    tableau.insertAdjacentHTML('beforeend', `<tr><td>${scores[i].nbrEssai}</td><td>${difficulte}</td><td style="background-color:${couleur}; color:#FFFFFF">${scores[i].status}</td></tr>`)
+    tableau.insertAdjacentHTML('beforeend', `<tr><td>Aucune partie joué</td><td>Aucune partie joué</td><td>Aucune partie joué</td></tr>`)
+
+} else {
+    //-- Ajoute au tableau les scores sauvegardé ------------
+    for(let i = 0; i < scores.length; i++) {
+
+        //-- Pour rendre le tableau plus beau ------------
+        switch(scores[i].difficulte) {
+            case 'bleusaille':
+                difficulte = 'Bleusaille';
+                break;
+        
+            case 'violence':
+                difficulte = 'Violence';
+                break;
+        
+            case 'ultraViolence':
+                difficulte = 'Ultra-Violence';
+                break;
+        
+            case 'cauchemar':
+                difficulte = 'Cauchemar';
+                break;
+        
+            case 'ultraCauchemar':
+                difficulte = 'Ultra-Cauchemar';
+                break;
+        }
+
+        //-- Change la couleur du fond pour qu'il soit plus compréhensible ------------
+        if(scores[i].status === 'Gagné') {
+            couleur = 'green';
+        } else {
+            couleur = 'red'
+        }
+
+        //-- Insert la ligne dans le tableau ------------
+        tableau.insertAdjacentHTML('beforeend', `<tr><td>${scores[i].nbrEssai}</td><td>${difficulte}</td><td style="background-color:${couleur}; color:#FFFFFF">${scores[i].status}</td></tr>`)
+    }
 }
